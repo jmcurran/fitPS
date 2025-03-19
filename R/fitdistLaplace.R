36423b03b7a00e9854b0c048237a7054c52c7069
jmcurran <james.m.curran@gmail.com>
2025-03-19 15:56:23 +0100

    Working laplace?

--- R/fitdistLaplace.R
+++ R/fitdistLaplace.R
@@ -1,11 +1,37 @@
-fitdistLaplace = function(x, nterms = 10, s, ...){
-  fit = fitdist(x, nterms = nterms, method = "mle", ...)
-  shape.hat = fit$shape
-  sigma.sq.hat = fit$var.shape
+fitdistLaplace = function(x, nterms = 10, ...){
+  ## This is just proof of concept at the moment so
+  ## the prior and its parameters are fixed to U[-2,2]
+  zlpp = function(shape){
+    -zetaloglikelihood(x, shape = shape) - log(zetaunifprior(shape))
+  }
+
+  start = 1.5
+
+  fit = optim(par = start,
+             fn = zlpp,
+             method = "L-BFGS-B",
+             lower = 0,
+             hessian = TRUE)
+
+  shape = fit$par ## NOTE VGAM's dzeta is parameterised in terms of
+  ## s = alpha - 1. This has consequences in the formula below
+  N = sum(x$data$rn)
+
+  vshape = function(shape){
+    z = VGAM::zeta(shape + 1)
+    zprime = VGAM::zeta(shape + 1, 1)
+    zprimeprime = VGAM::zeta(shape + 1, 2)
+    numer = ((shape + 1) * z)^2
+    denom = (shape + 1)^2 * N *(z * zprimeprime - zprime^2) + 1
+
+    return(numer / denom)
+  }
+
+  var.shape = vshape(shape)
 
   fl = function(s){
-    fitsPS:::zetalikelihood(x, shape = s) *
-      exp(-0.5 * (s - shape.hat)^2 / sigma.sq.hat)
+    exp(zlpp(shape = shape)) *
+      exp(-0.5 * (s - shape)^2 / var.shape)
   }
 
   return(fl)
